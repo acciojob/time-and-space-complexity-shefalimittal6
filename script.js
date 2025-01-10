97f4a6d503810b7c1af39fee1e4119699f17e22c
Shefali <mittal.shefali2000@gmail.com>
2025-01-10 21:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-// No JavaScript required for this task, but you can add dynamic behavior if needed in the future.
+// JavaScript can be added here for future enhancements
+console.log("Time and Space Complexity concepts loaded.");
